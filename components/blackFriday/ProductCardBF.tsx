598799518d19c7d0005d2b918c9efe9143710fef
leohx2 <leobgse@gmail.com>
2023-11-03 21:52:17 +0000

    countdown product style

--- components/blackFriday/ProductCardBF.tsx
+++ components/blackFriday/ProductCardBF.tsx
@@ -144,7 +144,7 @@ Props) {
             } ${l?.onMouseOver?.showCardShadow ? "lg:hover:card-bordered" : ""}
         ${
             l?.onMouseOver?.card === "Move up" &&
-            "duration-500 transition-translate ease-in-out lg:hover:-translate-y-2"
+            "duration-500 transition-translate ease-in-out md:hover:-translate-y-2"
         }
       `}
             data-deco="view-product"
--- components/blackFriday/ProductCountdownBF.tsx
+++ components/blackFriday/ProductCountdownBF.tsx
@@ -10,27 +10,30 @@ function ProductCountdownBF(props: Props) {
     const [days, hours, minutes, seconds] = useCountdown(countdown);
 
     return (
-        <div id="CountdownBF" class="flex gap-5">
-            <div>
-                <span class="countdown font-mono text-4xl">
+        <div
+            id="CountdownBF"
+            class="flex gap-3 lg:gap-4 text-xs md:text-sm lg:text-base"
+        >
+            <div class={`flex flex-col ${hours <= 0 ? "hidden" : ""}`}>
+                <span class="countdown font-mono text-xl md:text-2xl lg:text-3xl">
                     <span style={`--value:${days};`}></span>
                 </span>
                 days
             </div>
-            <div>
-                <span class="countdown font-mono text-4xl">
+            <div class="flex flex-col">
+                <span class="countdown font-mono text-xl md:text-2xl lg:text-3xl">
                     <span style={`--value:${hours};`}></span>
                 </span>
                 hours
             </div>
-            <div>
-                <span class="countdown font-mono text-4xl">
+            <div class="flex flex-col">
+                <span class="countdown font-mono text-xl md:text-2xl lg:text-3xl">
                     <span style={`--value:${minutes};`}></span>
                 </span>
                 min
             </div>
-            <div>
-                <span class="countdown font-mono text-4xl">
+            <div class="flex flex-col">
+                <span class="countdown font-mono text-xl md:text-2xl lg:text-3xl">
                     <span style={`--value:${seconds};`}></span>
                 </span>
                 sec
